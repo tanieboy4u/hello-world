4236d8494be67a46bd9c3fc3c17884fa92ac4308
tanieboy4u <sfdomingo@gmail.com>
2016-06-21 06:44:11 -0700

    Test:

---
+++ MVCTutorial/MVCMusic/MVCMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMusic.Startup))]
+namespace MVCMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
